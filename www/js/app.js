3b54ee87d963bb3f1054f5ad9f62b2e0a762c85b
葛方帅 <gefangshuai@163.com>
2015-08-06 17:11:31 +0800

    完成slide

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,56 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('app', ['ionic']);
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('home', {
+        url: '/home',
+        cache: 'false',
+        templateUrl: 'home.html',
+        controller: 'HomeCtrl'
+    });
+    $stateProvider.state('settings', {
+        url: '/settings',
+        cache: 'false',
+        templateUrl: 'settings.html',
+        controller: 'SettingsCtrl'
+    });
+    $urlRouterProvider.otherwise('/home');
+});
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+app.run(function ($ionicPlatform, $rootScope) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+    $rootScope.leftSide = 'home';
+});
+
+app.controller('HomeCtrl', function ($scope, $http, $ionicSideMenuDelegate) {
+    $scope.toggleLeft = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+});
+app.controller('SettingsCtrl', function ($scope, $http, $ionicSideMenuDelegate) {
+    $scope.toggleLeft = function () {
+        $ionicSideMenuDelegate.toggleLeft();
+    };
+});
+app.controller('LeftSideCtrl', function($rootScope, $scope, $ionicSideMenuDelegate,$ionicViewSwitcher, $state){
+    $scope.homeClick = function(){
+        $ionicViewSwitcher.nextDirection('no');
+        $state.go('home');
+        $rootScope.leftSide='home';
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+    $scope.settingsClick = function(){
+        $ionicViewSwitcher.nextDirection('no');
+        $state.go('settings');
+        $rootScope.leftSide='settings';
     }
-  });
-})
+});
