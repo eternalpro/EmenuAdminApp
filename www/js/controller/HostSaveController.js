0ccb2f95a0b22cd534a8e7788beeef3be71f0f5a
葛方帅 <gefangshuai@163.com>
2015-08-07 14:33:02 +0800

    完成初始化配置信息

--- www/js/controller/HostSaveController.js
+++ www/js/controller/HostSaveController.js
@@ -8,18 +8,18 @@ angular.module('app.hostSave', [
 ])
     .controller('HostSaveCtrl', function ($rootScope, $scope, $http, $localKey, $localStorage, $loading, $alert, $ionicModal) {
         $scope.saveHost = function (form) {
-            if(!form || !form.host || form.host.trim() == '') {
+            if (!form || !form.host || form.host.trim() == '') {
                 $alert.showAlert('请输入服务器IP！', '注意')
-            }else{
-                $loading.show('正在测试服务器的连通性...')
-                $http.get('http://' + form.host + '/api/system/sayHello').then(function(res){
-                    if(res.data == 'success'){
+            } else {
+                $loading.show('正在测试服务器的连通性...');
+                $http.get('http://' + form.host + '/api/system/sayHello').then(function (res) {
+                    if (res.data == 'success') {
                         $localStorage.set($localKey.host, form.host);
                         $alert.showAlert('设置成功！');
                         $rootScope.hostModal.hide();
                     }
                     $loading.hide();
-                }, function(response) {
+                }, function (response) {
                     $loading.hide();
                     $alert.showAlert('连接失败, 请重新输入！');
                 });
